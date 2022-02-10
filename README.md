48963276d9f1eb3b7b1c3567a2b8fe76fecf7aa6
vlaghe <82247523+vlagh3@users.noreply.github.com>
2022-02-10 15:47:18 +0200

    some clean-up

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Python library for accessing the [Midas](https://midas.investments/) API.
 
 > Note: this is not an official API provided by the project, but rather a work-around to access portofolio details through their [graphQl interface](https://api.midas.investments/)
+
+## Install
+- with pip:  `pip install -i https://test.pypi.org/simple/ midasB==0.1.0`
--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 from setuptools import setup, find_packages
 
+# Generate distribution archives
+#   python3 -m pip install --upgrade build
+#   python3 -m build
+# Upload to pypi: 
+#   python3 -m pip install --upgrade twine
+#   python3 -m twine upload --config-file .pypirc -r testpypi dist/*
+
+
+
 setup(
     name='midasB',
-    version='0.1.0',
+    version='0.0.1',
     author='vlaghe',
     author_email='vlaghe@protonmail.com',
     description='Unnoficial Midas API library for python',
     long_description_content_type="text/markdown",
     long_description=open("README.md", encoding="utf-8").read(),
     url='https://github.com/vlagh3/midas-bridge',
-    packages=find_packages()
-    install_requires=['requests', 'json'],
+    packages=find_packages(),
+    install_requires=['requests'],
     python_requires=">=3.6",
 )
